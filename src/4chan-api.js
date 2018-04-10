f5e4aa46f69a9ce14a29ea6c609c73cff8fe7e6a
0v3rl0w <0v3rl0w@gmail.com>
2018-04-10 15:47:14 +0200

    First Commit

---
+++ src/4chan-api.js
@@ -0,0 +1,12 @@
+const request = require("request");
+
+function getJson(link, callback)
+{
+    request({url: link, json: true}, function(error, response, body)
+    {
+        if(!error && response.statusCode === 200){
+           callback(body);
+        }
+    });
+}
+
---
+++ src/main-process/index.js
@@ -0,0 +1,34 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+const $ = require('jquery');
+
+function createWindow()
+{
+    win = new BrowserWindow({width: 1920, height: 1080, icon: path.join(__dirname, '../../gui/img/icon.png'), title: "Ya4r"});
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, '../../gui/index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    win.on('closed', () => {
+        win = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin') 
+    {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if(win === null)
+    {
+        createWindow();
+    }
+});
